01b89e86861a15077955bd41512bb95dc10d5596
francojbe <francojbe@gmail.com>
2026-05-07 21:45:07 -0400

    fix: ensure pretty URLs work on server with trailingSlash true

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,7 @@ const nextConfig: NextConfig = {
   // Configuración para permitir acceso desde dispositivos en la red local
   // @ts-ignore - propiedad requerida en Next 15+ para evitar error 403 en IPs locales
   allowedDevOrigins: ['192.168.1.83', 'better-kings-serve.loca.lt'],
+  trailingSlash: true,
 };
 
 export default nextConfig;
